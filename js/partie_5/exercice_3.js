d90cbff10f0433d0a08957faf36dc2a273516943
Lefort Steve <steve-ya@hotmail.com>
2023-05-04 16:51:06 +0200

    Exercice JS 06 fini

--- js/partie_5/exercice_3.js
+++ js/partie_5/exercice_3.js
@@ -1,3 +1,5 @@
 var C = window.prompt("Donnez la température en degrés Celsius");
 
+var F = (C*9/5) + 32
 
+document.write("Température convertie en degrés Fahrenheit : " + F);
---
+++ js/partie_6/exercice.js
@@ -0,0 +1,39 @@
+var chaine, a, b, c, d, e;
+
+chaine ="Ceci est une chaîne de caractère : ";
+
+document.write(chaine);
+
+a = "100";
+
+document.write(chaine + 100);
+
+b = 100;
+
+console.log("La valeur de b est :" + b);
+
+c = 1,00;
+
+console.log("La valeur de c est :" + c);
+
+d = true;
+
+console.log("d est :" + d);
+
+b = b--;
+
+console.log("J'ai appliquer la décrémentation a b qui donne :" + b--);
+
+c = c + a;
+
+console.log("La valeur de a c'est ajouter a celle de c ce qui donne :" + c + a )
+
+e = !d;
+
+console.log("La valeur de d c'est inversé :" + e)
+
+
+
+
+
+
